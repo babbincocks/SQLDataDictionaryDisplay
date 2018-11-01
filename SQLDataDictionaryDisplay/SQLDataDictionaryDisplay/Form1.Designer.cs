cbf31cb5614abbf740e2cb044fff55cc94c89136
babbincocks <zabbybab@gmail.com>
2018-10-31 21:40:51 -0400

    It fully functions on the class desktop computer. Need to resolve a display error when an item depends upon a scalar function, and make the program more portable (meaning find alternative to the literal connection string.)

---
+++ SQLDataDictionaryDisplay/SQLDataDictionaryDisplay/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SQLDataDictionaryDisplay
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbDatabases = new System.Windows.Forms.ComboBox();
+            this.lbDBItems = new System.Windows.Forms.ListBox();
+            this.lbDepending = new System.Windows.Forms.ListBox();
+            this.lbDependents = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cbDatabases
+            // 
+            this.cbDatabases.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDatabases.FormattingEnabled = true;
+            this.cbDatabases.Location = new System.Drawing.Point(29, 24);
+            this.cbDatabases.Name = "cbDatabases";
+            this.cbDatabases.Size = new System.Drawing.Size(243, 21);
+            this.cbDatabases.TabIndex = 0;
+            this.cbDatabases.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lbDBItems
+            // 
+            this.lbDBItems.FormattingEnabled = true;
+            this.lbDBItems.Location = new System.Drawing.Point(29, 69);
+            this.lbDBItems.Name = "lbDBItems";
+            this.lbDBItems.Size = new System.Drawing.Size(295, 394);
+            this.lbDBItems.TabIndex = 1;
+            this.lbDBItems.SelectedIndexChanged += new System.EventHandler(this.lbDBItems_SelectedIndexChanged);
+            // 
+            // lbDepending
+            // 
+            this.lbDepending.FormattingEnabled = true;
+            this.lbDepending.HorizontalScrollbar = true;
+            this.lbDepending.Location = new System.Drawing.Point(404, 69);
+            this.lbDepending.Name = "lbDepending";
+            this.lbDepending.Size = new System.Drawing.Size(527, 173);
+            this.lbDepending.TabIndex = 2;
+            // 
+            // lbDependents
+            // 
+            this.lbDependents.FormattingEnabled = true;
+            this.lbDependents.HorizontalScrollbar = true;
+            this.lbDependents.Location = new System.Drawing.Point(404, 290);
+            this.lbDependents.Name = "lbDependents";
+            this.lbDependents.Size = new System.Drawing.Size(527, 173);
+            this.lbDependents.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 508);
+            this.Controls.Add(this.lbDependents);
+            this.Controls.Add(this.lbDepending);
+            this.Controls.Add(this.lbDBItems);
+            this.Controls.Add(this.cbDatabases);
+            this.Name = "frmMain";
+            this.Text = "Database Dependencies";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbDatabases;
+        private System.Windows.Forms.ListBox lbDBItems;
+        private System.Windows.Forms.ListBox lbDepending;
+        private System.Windows.Forms.ListBox lbDependents;
+    }
+}
+
---
+++ SQLDataDictionaryDisplay/SQLDataDictionaryDisplay/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQLDataDictionaryDisplay
+{
+    public partial class frmMain : Form
+    {
+
+        private string selectedDB = "";
+
+        private SqlConnection sqlConn;
+
+        private string[] ignoredDBs = { "master", "tempdb", "model", "msdb" };
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (selectedDB != cbDatabases.SelectedItem.ToString())
+            {
+                lbDBItems.Items.Clear();
+                selectedDB = cbDatabases.SelectedItem.ToString();
+            }
+            using (sqlConn = new SqlConnection("Data Source=PL1\\MTCDB;Initial Catalog=" + selectedDB + "; TRUSTED_CONNECTION=True;"))
+            {
+                SqlCommand getTables = new SqlCommand("SELECT * FROM INFORMATION_SCHEMA.TABLES UNION SELECT ROUTINE_CATALOG, " +
+                    "ROUTINE_SCHEMA, ROUTINE_NAME, ROUTINE_TYPE FROM INFORMATION_SCHEMA.ROUTINES " +
+                    "WHERE ROUTINE_TYPE = 'PROCEDURE' ", sqlConn);
+                
+                SqlDataAdapter dbAdapt = new SqlDataAdapter(getTables);
+                DataTable tables = new DataTable();
+                dbAdapt.Fill(tables);
+
+                foreach (DataRow row in tables.Rows)
+                {
+                    string itemName = row[2].ToString();
+                    string itemSchema = row[1].ToString();
+                    if(itemSchema == "dbo")
+                    {
+                        lbDBItems.Items.Add(itemName);
+                    }
+                    else
+                    {
+                        lbDBItems.Items.Add(itemSchema + "." + itemName);
+                    }
+                        
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (sqlConn = new SqlConnection("Data Source=PL1\\MTCDB;Initial Catalog=master; TRUSTED_CONNECTION=True;"))
+                {
+
+                    SqlCommand getDB = new SqlCommand("SELECT name FROM sys.databases", sqlConn);
+                    SqlDataAdapter dbAdapt = new SqlDataAdapter(getDB);
+                    DataTable databases = new DataTable();
+                    dbAdapt.Fill(databases);
+                    
+                    foreach (DataRow row in databases.Rows)
+                    {
+                        string dbName = row[0].ToString();
+                        if (!ignoredDBs.Contains(dbName) && !dbName.Contains("ReportServer"))
+                        {
+                            cbDatabases.Items.Add(dbName);
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void lbDBItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lbDepending.Items.Clear();
+            lbDependents.Items.Clear();
+            DataSet dependencies = new DataSet();
+            string item = lbDBItems.SelectedItem.ToString();
+            try
+            {
+                using (sqlConn = new SqlConnection("Data Source=PL1\\MTCDB;Initial Catalog=" + selectedDB + "; TRUSTED_CONNECTION=True;"))
+                {
+                    SqlCommand dependProc = new SqlCommand("sp_depends", sqlConn);
+                    dependProc.CommandType = CommandType.StoredProcedure;
+                    SqlParameter itemName = new SqlParameter("@objname", item);
+                    dependProc.Parameters.Add(itemName);
+
+                    SqlDataAdapter dbAdapt = new SqlDataAdapter(dependProc);
+
+                    dbAdapt.Fill(dependencies);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if(dependencies.Tables.Count == 0)
+            {
+                lbDependents.Items.Add(item + " has no items depending upon it.");
+                lbDepending.Items.Add(item + " has no dependencies upon other items.");
+            }
+            else if(dependencies.Tables.Count == 1)
+            {
+                if(dependencies.Tables[0].Columns.Count == 2)
+                {
+                    lbDepending.Items.Add(item + " has no dependencies upon other items.");
+
+                    foreach(DataRow row in dependencies.Tables[0].Rows)
+                    {
+                        lbDependents.Items.Add(item + " has the \"" + row[0].ToString() + "\" " + row[1].ToString() + " dependent upon it.");
+                    }
+                    
+
+                }
+                else if(dependencies.Tables[0].Columns.Count == 5)
+                {
+                    lbDependents.Items.Add(item + " has no items depending upon it.");
+
+                    foreach(DataRow row in dependencies.Tables[0].Rows)
+                    {
+                        lbDepending.Items.Add(item + " is dependent upon the \"" + row[4].ToString() + "\" column of the " + row[0].ToString() + " " + row[1].ToString() + ".");
+                    }
+                }
+            }
+            else
+            {
+                foreach(DataRow row in dependencies.Tables[0].Rows)
+                {
+                    lbDepending.Items.Add(item + " is dependent upon the \"" + row[4].ToString() + "\" column of the " + row[0].ToString() + " " + row[1].ToString() + ".");
+
+                }
+                foreach (DataRow row in dependencies.Tables[1].Rows)
+                {
+                    lbDependents.Items.Add(item + " has the \"" + row[0].ToString() + "\" " + row[1].ToString() + " dependent upon it.");
+
+                }
+            }
+        }
+    }
+}
